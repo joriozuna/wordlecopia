c9732adb7ed9bbd232f6d10121a2ab4ae956e48d
joriozuna <138429041+joriozuna@users.noreply.github.com>
2024-06-16 16:32:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+
+const palabras5Letras = ["vamos","ahora","estoy","tengo","estás","puedo","tiene","bueno","hacer","todos","puede","sabes","quién","nunca","dónde","favor","señor","mejor","están","usted","mucho","hasta","sobre","decir","cosas","antes","estar","noche","nadie","padre","gente","donde","mismo","hecho","ellos","claro","estas","lugar","mundo","amigo","desde","fuera","tener","crees","buena","gusta","nuevo","había","menos","tenía","madre","quien","luego","todas","mujer","visto","haces","tarde","parte","haber","saber","veces","tanto","razón","quizá","estos","salir","hemos","chica","entre","algún","serio","somos","pensé","debes","forma","chico","dicho","nueva","sabía","ayuda","hacia","miedo","adiós","poder","niños","sería","viejo","manos","pasar","viene","horas","listo","único","cerca","otros","sigue","habla","feliz","llama","venir","morir","atrás","dices","abajo","dejar","tomar","justo","juego","matar","cinco","dicen","clase","pueda","igual","venga","creer","saben","hagas","comer","punto","misma","vivir","queda","hijos","mayor","hacen","medio","basta","meses","fácil","final","lista","trata","armas","podía","perro","fuego","murió","verte","culpa","veras","jugar","joven","tenga","única","lejos","digas","vemos","trato","jamás","falta","lleva","gusto","coche","mucha","cielo","habrá","orden","deben","libro","calle","clark","libre","viste","llamo","sueño","viaje","peter","resto","avión","frank","negro","miren","haría","acaba","otras","dolor","demás","poner","tonto","campo","acabó","sitio","verlo","error","llegó","mente","novia","suena","harry","hazlo","grupo","perra","acabo","traje","james","largo","entra","línea","barco","ganar","mitad","lindo","pagar","fotos","pobre","tipos","darle","amiga","vista","salvo","hotel","señal","llevo","ellas","honor","corre","jimmy","hablo","media","estan","linda","norte","busca","siete","llave","santo","irnos","vayas","salió","pelea","banco","calma","creen","hacía","novio","decía","deseo","carne","vidas","jesús","creía","carta","estés","david","quise","ambos","dulce","vayan","vuelo","harás","junto","diría","suelo","llega","verás","prisa","según","broma","sacar","papel","llamó","hagan","dejes","duele","causa","luces","ojalá","radio","brazo","cargo","hogar","corte","baile","vieja","rayos","serás","salud","parar","cenar","serán","volar","nivel","bolsa","acaso","mirar","salga","perdí","santa","miles","bomba","darme","verla","bobby","hayas","mando","henry","damas","robot","llevó","muere","trago","reina","lleno","valor","héroe","capaz","verme","beber","darte","abrir","golpe","tenia","tocar","ganas","pedir","pista","sirve","doble","estén","debía","eddie","color","aquel","fondo","chris","verde","traer","pidió","éxito","hielo","podrá","oigan","común","calor","pared","subir","llame","bajar","socio","danny","dando","larga","solía","solos","tanta","oeste","parís","carga","banda","dejas","nueve","llena","tommy","dejen","reloj","nariz","vengo","billy","gordo","ayude","locos","cinta","desea","leyes","árbol","hable","malas","joder","lleve","ruido","jason","líder","harán","ponte","bella","sarah","costa","debió","araña","podré","total","lucha","leche","diste","jerry","ponga","lados","trajo","deber","débil","quedó","quiso","carro","dedos","sigan","dueño","turno","caray","quede","playa","primo","oíste","silla","envió","steve","abran","sentí","casas","mueve","video","yendo","vivos","grave","pecho","sepas","modos","seria","laura","apoyo","toque","larry","hayan","marca","hablé","serie","paseo","balas","susan","sabia","negra","quedo","pulso","bordo","época","entró","pesar","habló","llamé","cerdo","busco","tumba","china","datos","torre","robar","malos","techo","genio","ideas","tomas","menor","droga","gustó","scott","mires","huele","pocos","ángel","local","chloe","vives","virus","maría","nieve","fumar","vimos","reino","clave","plata","magia","humor","autos","julie","curso","canal","echar","frodo","corto","llego","cable","helen","contó","siglo","roger","andar","pasan","troya","diles","coger","lunes","tarea","corta","truco","pollo","tonta","pongo","sucio","habia","pasos","guste","polvo","pones","firma","texas","jones","firme","flota","queso","shrek","muero","adios","viven","sonny","guapo","smith","fiona","trató","ricos","tomen","crear","extra","fuese","actor","naves","meter","dejan","vacío","salón","motor","mueva","hazme","bravo","regla","cuida","ponen","brian","niñas","bruja","vegas","kevin","paris","túnel","pensó","copia","alice","molly","carol","legal","solas","chino","arena","casos","miras","seres","disco","traté","daría","placa","notas","creas","verán","salvó","quita","museo","aviso","leído","tirar","traes","rocky","rumbo","muera","fecha","digan","maten","algun","julio","ciego","súper","jenny","emily","ethan","oscar","sabrá","pocas","volví","clima","adoro","suave","bolso","betty","vivía","juega","acabe","guapa","botón","ruego","nadar","mover","conde","pedro","celda","amaba","nació","manda","salgo","metas","lenny","ricky","burro","louis","grial","usado","cajas","matan","pieza","salen","pizza","junta","caído","ducha","venía","pagan","ambas","dudas","ritmo","venta","almas","quedé","kilos","serlo","plato","bolas","falso","mandó","botas","grado","daños","minas","ratas","julia","pense","sales","aldea","obvio","peces","envía","karen","gatos","éstas","éstos","caras","tigre","harto","lento","hecha","simon","cruel","dirás","casar","falla","annie","verle","donna","noble","laden","white","metro","grace","gorda","paren","damos","grant","cohen","tomes","sandy","salta","rusos","punta","pasen","saint","papas","marco","terry","salsa","metió","marty","tetas","sabor","bello","pasas","japón","acero","miami","huevo","trate","casco","ayudó","marie","katie","razon","buddy","globo","ayudo","reyes","seiya","sally","kelly","wayne","canta","bebés","india","brown","crema","barry","matas","circo","salve","logro","unión","móvil","cañón","bruce","sepan","falsa","black","rocas","putas","judío","saque","metal","apaga","deuda","cueva","aérea","bajen","aaron","temas","calla","wyatt","sonar","patio","hueso","zorra","valle","logró","creyó","nicky","civil","velas","selva","conté","vende","caída","vuela","rubia","joyas","amado","furia","motel","ideal","piano","lewis","letra","dosis","dólar","benny","apolo","super","rifle","rueda","espía","salto","clara","grano","odias","rusia","salva","lanza","moral","bajas","moscú","barra","comen","piper","dimos","unico","prima","actúa","breve","tuyos","plaza","darán","robin","toman","suban","borde","polly","abrió","shaun","suyos","grasa","revés","nancy","trozo","atras","ponlo","pasta","pinta","sucia","hijas","tracy","rompe","entré","césar","holly","burke","jaula","fatal","ronda","cobra","aéreo","monje","andas","teddy","llevé","entro","pares","oídos","dieta","guión","enano","cindy","rezar","cortó","jenna","sammy","cazar","bodas","rosas","seguí","oírlo","davis","junio","linea","costó","temer","rollo","envié","atacó","sigas","radar","carla","lobos","citas","preso","telly","cuero","obras","vacía","jesse","saqué","lucas","diosa","abril","rosie","oreja","villa","trono","traen","salía","raras","lavar","angie","viera","rango","niega","mates","shock","renta","frase","mirad","jesus","aguas","ralph","quema","poema","cabra","voces","temor","pague","comió","nubes","ánimo","gafas","aguja","corea","pases","mitch","megan","logan","facil","diera","comes","ciega","dinos","presa","patas","oírme","myers","ataca","tenis","plano","sobra","rumor","metes","besar","monte","digno","darse","woody","paige","green","altos","tyler","vacas"];
+
+
+const buttonElements = document.querySelectorAll('button');
+let row = 1;
+let letter = 1;
+const wordForTheDay = palabras5Letras[Math.floor(Math.random()*palabras5Letras.length)];
+const wordElements = document.querySelectorAll('.words-row');
+let gameOver = false;
+let guessedCorrectly = false;
+
+
+
+buttonElements.forEach((element) => {
+    element.addEventListener('click', function() {
+        keypress(element.attributes["data-key"].value)
+    } )
+});
+
+function populateWord(key){
+    if (letter < 6){
+    wordElements[row-1].querySelectorAll('.word')[letter-1].innerText = key;
+    letter += 1;
+    }
+}
+
+function checkWord(){
+    const letterElements = wordElements[row-1].querySelectorAll('.word');
+    let numOfCorrectAlphabets = 0;
+
+    letterElements.forEach((element, index) => {
+        const indexOfLetterInWordOfTheDay = wordForTheDay.toLowerCase().indexOf(element.innerText.toLowerCase());
+    
+    if(indexOfLetterInWordOfTheDay === index){
+        numOfCorrectAlphabets +=1;
+        element.classList.add('word-green');
+    } else if(indexOfLetterInWordOfTheDay > 0) {
+        element.classList.add('word-yellow');
+    } else { element.classList.add('word-grey')}
+});
+if (numOfCorrectAlphabets === 5){
+    gameOver = true;
+    guessedCorrectly = true;
+    alert('Felicidades, has acertado!');
+}else if(row === 6){
+    gameOver = true;
+    alert('Has utilizado todos tus intentos, la palabra era ' + wordForTheDay)
+};
+}
+
+function enterWord(){
+    if(letter < 6){
+        alert('Introduce mas letras');
+    }else {
+        checkWord();
+        row +=1;
+        letter = 1;
+    }
+}
+
+function deleteLetter(){
+    const letterElements = wordElements[row-1].querySelectorAll('.word');
+    for (let index = letterElements.length - 1; index >= 0; index--){
+        const element = letterElements[index];
+        if(element.innerText !== ''){
+            element.innerText = '';
+            letter -=1;
+            break
+        }
+    }
+}
+
+function keypress(key){
+    if(gameOver == false){
+    if(key.toLowerCase() === 'enter'){
+        enterWord();
+    }else if (key.toLowerCase() === 'del'){
+        deleteLetter();
+    }else{
+        populateWord(key);
+    }
+    }
+    else{
+        alert('Intenta de nuevo la proxima')
+    }
+}
